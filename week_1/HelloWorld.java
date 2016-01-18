1bd12e9934d17e67da5fc5c98d10b34a3e798af7
Brian Long <brian.long@firstmoversadvantage.com>
2016-01-17 18:59:35 -0700

    Added some comments.

--- week_1/HelloWorld.java
+++ week_1/HelloWorld.java
@@ -3,17 +3,14 @@ public class HelloWorld {
     System.out.println("Hello World");
   }
 }
-
-
-
-
-// public
-//     Denotes that the method is visible to every part of the program.
-// static
-//     Denotes that there is only one of this method.
-// void
-//     Specifies the return type of the method. As this is the method for our entry-point into the program, this must be void as it does not return anything.
-// main
-//     This is the name of the method is the entry-point into our program.
-// String[] args
-//     Inside the parentheses we get the parameter of the method; this indicates any arguments that are passed in from the command line. We’ll use these below.
+// public        - Denotes that the method is visible to every part of the
+//                 program.
+// static        - Denotes that there is only one of this method.
+// void          - Specifies the return type of the method. As this is the
+//                 method for our entry-point into the program, this must be
+//                 void as it does not return anything.
+// main          - This is the name of the method is the entry-point into our
+//                 program.
+// String[] args - Inside the parentheses we get the parameter of the method;
+//                 this indicates any arguments that are passed in from the
+//                 command line.
---
+++ week_1/README.md
@@ -0,0 +1,7 @@
+1. The code for HelloWorld.java was provided by the lab notes.
+- To compile: `javac HelloWorld.java`
+- To run: `java HelloWorld`
+2. The code for HelloWorldTest.java was provided by the lab notes.
+- See junit_notes.md for comments on installing the Junit jar files & CLASSPATH
+- To compile: `javac HelloWorldTest.java`
+- To run: `java org.junit.runner.JUnitCore HelloWorldTest`
